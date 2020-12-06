a8a21ad3a280e4ec16b3c5a5b6cee00d61b751f4
BakaDobe <anthoniuspowellus@gmail.com>
2020-12-06 17:29:58 -0500

    Added new level, and a lot of actors

--- Runaway-Racer-main/Runaway Racer/MyWorld.java
+++ Runaway-Racer-main/Runaway Racer/MyWorld.java
@@ -14,5 +14,15 @@ public class MyWorld extends World
     public MyWorld()
     {
         super(600, 400, 1);
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        
     }
 }
